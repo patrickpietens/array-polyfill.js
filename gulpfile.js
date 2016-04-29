e335290198d2c64e5e3d4210d53d6ac7e26f91f2
Patrick Pietens <hello@patrickpietens.com>
2016-04-29 16:52:03 +0200

    Rename source file fixing import issues

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ function onError() {
 gulp.task('default', () => {
     let myProduction = process.env.NODE_ENV === 'production';
 
-    let mySource = 'src/array-polyfill.js',
+    let mySource = 'src/array.js',
         myDestination = 'dist';
 
     let myUmdOptions = {
